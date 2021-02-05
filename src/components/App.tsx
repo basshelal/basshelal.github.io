52cfc266c23e1a68b416751366fc9d83393c5ad6
Bassam Helal <basshelal@gmail.com>
2021-02-05 02:14:47 +0000

    Added basic React TypeScript Webpack functionality I think

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import {Component, ReactNode} from "react"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+
+export default class App extends Component<{}, {}> {
+
+    constructor(props: {}) {
+        super(props)
+    }
+
+    public render(): ReactNode {
+
+        return (
+            <BrowserRouter>
+                <div>
+                    <h1>Hello World!</h1>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
