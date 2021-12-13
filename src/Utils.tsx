777272f3ec621a4df48d16f10db13675f6031e28
Bassam Helal <basshelal@gmail.com>
2021-12-13 23:24:26 +0300

    More work on Blog and the architecture surrounding it, will be a little tricky but I think I have a decent sketch of a good idea

--- src/Utils.tsx
+++ src/Utils.tsx
@@ -6,3 +6,7 @@ export function state<S>(initialState: S | (() => S)): { get: S, set: Dispatch<S
     const [state, setState] = useState<S>(initialState)
     return {get: state, set: setState}
 }
+
+export function newDate(date: { year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number }): Date {
+    return new Date(date.year, date.month, date.date, date.hours, date.minutes, date.seconds, date.ms)
+}
--- src/pages/blog/BlogView.tsx
+++ src/pages/blog/BlogView.tsx
@@ -1,20 +1,24 @@
 import React, {FC} from "react"
 import {P} from "../../Utils"
 
-// Must be serializable because this will be stored in a json or yaml file
 export interface BlogPost {
-    title: string,
-    url: string,
-    datePublished: number,
-    dateLastModified?: number,
-    subtitle?: string,
-    tags: Array<string>,
-    componentFile: string, // the file containing the react component that will be rendered or shown
-    contentFile: string, // the file containing the content that the react component will use to render the content,
-                         // this is like a .md file
+    /** Title of the post, ideally should not be modified after publish*/
+    readonly title: string,
+    /** Unique identifier of the post, this is used for the url and should NEVER change, even if everything else changed */
+    readonly id: string,
+    /** Date the post was published, should never be modified */
+    readonly datePublished: Date,
+    /** Date the post was last modified, if never modified then null and will not show in the view */
+    readonly dateLastModified?: Date,
+    /** Subtitle, a small sentence like a blurb under the title */
+    readonly subtitle?: string,
+    /** Tags this post belongs to, this can (but should not) be empty */
+    readonly tags: Array<string>,
+    /** The path of the file of the contents of this post, we will use .md (Markdown) files */
+    readonly contentFilePath: string,
 }
 
-// View receives this from the list (which scanned all posts) and this used to render the whole page
+// View receives this from the list (which scanned all posts) and this is used to render the whole page
 export interface BlogViewProps {
     blogPost: BlogPost
 }
@@ -26,8 +30,6 @@ export const BlogView: FC<BlogViewProps> = (props: P<BlogViewProps>) => {
     // estimated read-time? related to word count https://infusion.media/content-marketing/how-to-calculate-reading-time/
     // tags, each clickable
     // Content (can contain headings & subheadings, images and code blocks, table of contents inside the content)
-    // Previous Post?
-    // Next Post?
 
     return (<>
     </>)
