5b21184d1d2edf687a411cdedfbf1264e57ad0d0
Bassam Helal <basshelal@gmail.com>
2022-08-01 15:46:09 +0100

    New CV PDF and rearranged links to be GitHub, email, LinkedIn

--- src/pages/cv/components/Header.tsx
+++ src/pages/cv/components/Header.tsx
@@ -55,11 +55,11 @@ export const Header: FC = (props: P) => {
         }}>
             <a href="https://github.com/basshelal" target="_blank" title="Github profile" style={{margin: "16px"}}>
                 <img alt="Github profile" height="50" width="50" src="../../../res/images/github.svg"/></a>
+            <a href="mailto:basshelal@gmail.com" target="_blank" title="Email" style={{margin: "16px"}}>
+                <img alt="Email" height="50" width="50" src="../../../res/images/email.svg"/></a>
             <a href="https://www.linkedin.com/in/bassamhelal" target="_blank" title="LinkedIn profile"
                style={{margin: "16px"}}>
                 <img alt="LinkedIn profile" height="50" width="50" src="../../../res/images/linkedin.svg"/></a>
-            <a href="mailto:basshelal@gmail.com" target="_blank" title="Email" style={{margin: "16px"}}>
-                <img alt="Email" height="50" width="50" src="../../../res/images/email.svg"/></a>
         </div>
     </>)
 }
