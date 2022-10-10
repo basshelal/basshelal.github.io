36eb009bbb44b480a6f282075dda6f3f798c2334
Bassam Helal <basshelal@gmail.com>
2022-10-10 13:45:32 +0100

    CV is now directly printable in order to maintain only 1 copy, the online copy, from which a PDF can be directly generated

--- src/pages/cv/CV.tsx
+++ src/pages/cv/CV.tsx
@@ -1,25 +1,14 @@
 import * as React from "react"
 import {FC} from "react"
-import {Header} from "./components/Header"
+import {SimpleHeader} from "./components/SimpleHeader"
 import {EducationSection, InterestsSection, SoftSkillSection, TechnicalSkillsSection} from "./components/Sections"
-import {Button} from "@material-ui/core"
-import {PictureAsPdf} from "@material-ui/icons"
 
 export const CV: FC = () => {
     return (<>
-        <Header/>
+        <SimpleHeader/>
         <TechnicalSkillsSection/>
         <SoftSkillSection/>
         <EducationSection/>
         <InterestsSection/>
-        <div style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingTop: "4px"
-        }}><a title="Printable PDF Version" href="res/Bassam-Helal-CV.pdf" target="_blank">
-            <Button style={{color: "#880E4F"}}><PictureAsPdf style={{fontSize: "3rem"}}/></Button>
-        </a>
-        </div>
     </>)
 }
---
+++ src/pages/cv/components/SimpleHeader.tsx
@@ -0,0 +1,51 @@
+import React, {AnchorHTMLAttributes, FC, PropsWithChildren} from "react"
+import {P} from "../../../Utils"
+import styled from "styled-components"
+
+export const SimpleHeader: FC = (props: P) => {
+
+    const NameHeading = styled.h1`{
+      font-family: "Roboto", sans-serif;
+      font-weight: 100;
+      font-size: 6rem;
+      color: #880E4F;
+      padding: 0;
+      margin: 0;
+    }`
+
+    const StyledLink = styled.a`{
+      margin: 8px 16px 16px 16px;
+      font-family: "Roboto", sans-serif;
+      font-weight: 300;
+      font-size: 1.2rem;
+    }`
+
+    const Link = (props: PropsWithChildren<AnchorHTMLAttributes<any>>): React.ReactElement => {
+        return (<StyledLink target="_blank" {...props}>{props.children}</StyledLink>)
+    }
+
+    return (<>
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            alignContent: "end",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "0px"
+        }}>
+            <NameHeading>Bassam Helal</NameHeading>
+        </div>
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "0"
+        }}>
+            <Link href="https://github.com/basshelal" title="Github">github.com/basshelal</Link>
+            <Link href="mailto:basshelal@gmail.com" title="Email">basshelal@gmail.com</Link>
+            <Link href="https://www.linkedin.com/in/bassamhelal" title="LinkedIn">linkedin.com/in/bassamhelal</Link>
+        </div>
+    </>)
+}
