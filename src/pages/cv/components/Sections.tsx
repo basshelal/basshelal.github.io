043481bb497913c9e0a5dc2b7611465cea666375
Bassam Helal <basshelal@gmail.com>
2023-04-25 21:15:02 +0100

    Updated CV

--- src/pages/cv/components/Sections.tsx
+++ src/pages/cv/components/Sections.tsx
@@ -58,14 +58,14 @@ export const Section = (props: PropsWithChildren<SectionProps>) => {
 
 export const TechnicalSkillsSection = () => {
     return (<Section title="Technical Skills">
-        <I>Advanced proficiency in <H>ANSI C</H> for <H>Embedded Systems</H></I>
+        <I>Advanced proficiency in <H>C</H> for <H>Embedded Systems</H> and <H>Systems Programming</H></I>
         <I>Advanced proficiency in <H>Kotlin</H> and <H>Java</H> for <H>JVM</H> development</I>
         <I>Advanced proficiency in <H>TypeScript</H> for <H>Node.js</H></I>
         <I>Intermediate proficiency in <H>Embedded Systems</H> concepts and protocols</I>
         <I>Intermediate proficiency in <H>bash</H> scripting and <H>GNU/Linux</H></I>
-        <I>Intermediate proficiency in <H>Android Development</H></I>
-        <I>Intermediate proficiency in <H>React.js</H></I>
-        <I>Basic proficiency in <H>Assembly Language</H> (x86 and ARM)</I>
+        <I>Intermediate proficiency in <H>Android Development</H> using <H>Kotlin</H></I>
+        <I>Intermediate proficiency in <H>React.js</H> using <H>TypeScript</H></I>
+        <I>Basic proficiency in reading <H>Assembly Language</H> (x86 and ARM)</I>
         <I>Basic proficiency in <H>(Embedded) C++</H></I>
         <br/>
         <I>Extensive experience using <H>Test Driven Development</H></I>
@@ -73,7 +73,7 @@ export const TechnicalSkillsSection = () => {
         <I>Extensive experience with <H>Functional Programming</H> concepts and principles</I>
         <I>Moderate experience using various embedded platforms
             such as <H>ESP32</H> and <H>Raspberry Pi Pico</H></I>
-        <I>Moderate experience building and updating <H>Full-Stack Web Apps</H></I>
+        <I>Moderate experience building and maintaining <H>Full-Stack Web Apps</H></I>
         <I>Moderate experience <H>contributing to Open-Source Projects</H></I>
         <I>Limited experience with <H>Project Planning and Management</H></I>
     </Section>)
@@ -94,6 +94,7 @@ export const EducationSection = () => {
             <I>Operating Systems and Architectures</I>
             <I>Advanced Object Oriented Programming</I>
             <I>Algorithms & Data Structures</I>
+            <I>Declarative & Functional Programming</I>
             <I>Concurrency</I>
             <I>Software Testing</I>
             <I>Web Application Development</I>
@@ -103,25 +104,25 @@ export const EducationSection = () => {
 
 export const SoftSkillSection = () => {
     return (<Section title="Soft Skills">
-        <I>Strong drive and enthusiasm for high quality, <H>scalable and maintainable software</H></I>
-        <I><H>Fast learner</H> with a deep passion for learning new things, not just in software</I>
-        <I>Adeptness in <H>project planning and management</H></I>
-        <I>Effective <H>teamwork</H> and <H>communication</H> skills</I>
-        <I><H>Flexible and adaptable</H> to changing project requirements</I>
-        <I><H>Open-mindedness</H> and willingness to explore new ideas and solutions</I>
-        <I><H>Culturally diverse</H> from frequent moving across continents during youth</I>
-        <I>Native fluency in <H>English</H> and <H>Arabic</H>, basic proficiency
-            in <H>Turkish</H> and <H>Persian</H></I>
+        <I>Strong drive to build <H>scalable and maintainable software</H></I>
+        <I><H>Passion for learning</H> new things, not just in software</I>
+        <I>Effective <H>teamwork and communication</H> skills</I>
+        <I><H>Strong presentation skills</H> even for complex technical concepts</I>
+        <I><H>Adaptability and flexibility</H> to changing project requirements</I>
+        <I><H>Open-mindedness</H> and willingness to explore novel solutions and ideas</I>
+        <I>Strong proficiency in <H>reading and writing documents</H></I>
+        <I>Native fluency in <H>English</H> and <H>Arabic</H>, learner proficiency
+            in literary <H>Persian</H> and <H>Turkish</H></I>
     </Section>)
 }
 
 export const InterestsSection = () => {
     return (<Section title="Interests">
-        <I>I am a passionate advocate for Free (as in Freedom) software.
+        <I>I am a passionate advocate for Free (as in Freedom) software,
             I use GNU/Linux as my daily operating system.</I>
         <I>I enjoy exploring and listening to a wide range of music from across the world.
-            I play the keyboard and qanun in my free time.</I>
+            I play the keyboard and Qanun in my free time.</I>
         <I>I enjoy linguistics and learning about different languages,
-            I am a regular contributor to Wiktionary in my spare time.</I>
+            I am currently learning Classical Persian and am a regular contributor to Wiktionary in my spare time.</I>
     </Section>)
 }
